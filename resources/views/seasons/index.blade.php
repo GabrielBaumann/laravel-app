eff627f12f24c2d950d01ce59d8dd5c0e7b1138c
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-06-04 10:19:37 -0300

    Update: Add title & optmized code

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -1,12 +1,14 @@
-<x-layout title='Séries'>
-    
+<x-layout title='Temporadas de {!! $series->nome !!}'>
+    <div class='mb-5'>
+        <h1 class='text-gray-200 text-3xl font-semibold'>Temporadas de '{!! $series->nome !!}'</h1>
+    </div>
     <div>
         <ul class='border border-b-0 border-gray-700 rounded-xl overflow-hidden bg-gray-800'>
             @foreach ($seasons as $season)
             <li class='flex items-center justify-between transition-all hover:bg-gray-700 hover:shadow-xl p-2 text-md text-gray-200 border-b border-gray-700'>
                 Temporada {{ $season->number }}
                 <span class='flex items-center gap-5'>
-                    {{ $season->episodes->count() }}
+                    {{ $season->episodes->count() }} Episódios 
                 </span>
             </li>
             @endforeach
